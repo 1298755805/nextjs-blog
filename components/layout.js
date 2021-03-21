2dc99195faa4e4f4bc47623487ffcd7c87c13316
yangmingxu <yangmingxu.bk201@bytedance.com>
2021-03-21 14:26:03 +0800

    fix: feat-demo

--- components/layout.js
+++ components/layout.js
@@ -3,8 +3,8 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'Your Name';
-export const siteTitle = 'Next.js Sample Website';
+const name = 'YMX';
+export const siteTitle = 'Welcome to my Demo';
 
 export default function Layout({ children, home }) {
   return (
